c81e52eb4df57d13dadb5bd4868a6e63f1cbeb63
Gustavo Miguel <gustavocarranco1@gmail.com>
2023-01-30 18:05:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco, total;
+            int quantidade;
+            string descricao;
+
+            Console.Write("Informe a descrição do produto: ");
+            descricao = Console.ReadLine();
+            Console.Write("\nQuantidade Solicitada: ");
+            quantidade = int.Parse(Console.ReadLine());
+            Console.Write("\nPreço do produto escolhido: ");
+            preco = double.Parse(Console.ReadLine());
+
+            total = quantidade * preco;
+
+            Console.WriteLine("\nValor a pagar: " + total);
+            Console.ReadKey();
+        }
+    }
+}
